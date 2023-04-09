7fbbf7f1fc39aa7e1190ba49a78f9eb0cdb6d3af
ShriyashBPatil <shriyashbp22hcs@student.mes.ac.in>
2023-04-09 12:35:52 +0530

    multi

--- passwordstudent.js
+++ passwordstudent.js
@@ -1,9 +1,18 @@
 
+		var users = {
+			"user1": "password1",
+			"user2": "password2",
+			"user3": "password3"
+		};
+
 		function authenticate() {
+			
 			var username = document.getElementById("username").value;
 			var password = document.getElementById("password").value;
 
-			if (username === "admin" && password === "password123") {
+			
+			if (users[username] && users[username] === password) {
+				alert("Login successful!");
 				window.location.href = "red.html"; 
 				return false; 
 			} else {
