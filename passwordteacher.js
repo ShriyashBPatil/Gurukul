410bf80bcf1ca692a70ae358a455e4017591610a
ShriyashBPatil <shriyashbp22hcs@student.mes.ac.in>
2023-04-09 12:01:36 +0530

    removecom

--- passwordteacher.js
+++ passwordteacher.js
@@ -1,15 +1,14 @@
 function authenticate() {
-    // Get the username and password from the form
+    
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
-    // Replace the following lines with your authentication logic
+   
     if (username === "admin" && password === "password123") {
-        alert("Login successful!");
-        window.location.href = "home.html"; // Redirect to welcome page
-        return false; // Prevent the form from being submitted
+        window.location.href = "home.html"; 
+        return false;
     } else {
         alert("Invalid username or password. Please try again.");
-        return false; // Prevent the form from being submitted
+        return false; 
     }
 }
