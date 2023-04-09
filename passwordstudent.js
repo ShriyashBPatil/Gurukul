a59b8daf25bbc2c977a69502f0e92fe01722a03e
ShriyashBPatil <shriyashbp22hcs@student.mes.ac.in>
2023-04-09 11:46:40 +0530

    commt

---
+++ passwordstudent.js
@@ -0,0 +1,15 @@
+
+		function authenticate() {
+			var username = document.getElementById("username").value;
+			var password = document.getElementById("password").value;
+
+			// Replace the following lines with your authentication logic
+			if (username === "admin" && password === "password123") {
+				alert("Login successful!");
+				window.location.href = "home.html"; // Redirect to welcome page
+				return false; // Prevent the form from being submitted
+			} else {
+				alert("Invalid username or password. Please try again.");
+				return false; // Prevent the form from being submitted
+			}
+		}
